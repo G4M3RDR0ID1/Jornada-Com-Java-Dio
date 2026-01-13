f5184e818923d1dc819a6ae11bee7800ae2f278d
G4M3RDR0ID1 <gamerdroid33@hotmail.com>
2026-01-13 13:44:48 -0300

    initial commit: SmartTv com java para estudo de metodos

---
+++ metodos/smartTv/src/Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.ligar();
+        smartTv.diminuirVolume();
+        smartTv.statusDaTv();
+    }
+}
